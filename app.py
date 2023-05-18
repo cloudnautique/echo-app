0cb65e7642c6ca062b0a9afbd52aff3c929136ac
Bill Maxwell <cloudnautique@users.noreply.github.com>
2023-05-18 16:08:45 -0700

    updated to use a password
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import mimetypes
 import redis
+import os
 
 from types import NoneType
 from flask import Flask, render_template, request
 
 from natsort import natsorted, ns
 
+redis_pass=os.getenv('REDIS_PASS')
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+cache = redis.Redis(host='redis', port=6379, db=0, password=redis_pass, decode_responses=True)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
